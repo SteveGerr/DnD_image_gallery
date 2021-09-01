6e814044ad7c8bc2bd63ded23cd06018e838a506
SteveGerr <prikolsamara@mail.ru>
2021-09-01 20:09:10 +0400

    feat: add remove method, emit removeImg

--- src/components/Picture/Picture.vue
+++ src/components/Picture/Picture.vue
@@ -3,6 +3,14 @@
         <img
             class="picture__img"
             :src="src">
+        <b-button
+            class="picture__close-btn"
+            @click="remove"
+            size="sm"
+            variant="dark"
+        >
+        x
+        </b-button>
     </div>
 </template>
 
@@ -18,6 +26,9 @@ export default {
     methods: {
         getWidthPicture(e) {
             this.$emit('width', e)
+        },
+        remove() {
+            this.$emit('removeImg')
         }
     }
 }
@@ -32,6 +43,12 @@ export default {
         max-height: 1000px;
         height: fit-content;
         margin: 10px;
+        position: relative;
+        &:hover {
+            .picture__close-btn {
+                display: flex;
+            }
+        }
 
         @media (max-width: 767px) {
             max-width: 100%;
@@ -41,5 +58,11 @@ export default {
     .picture__img {
         max-width: 100%;
         object-fit: contain;
+    }
+    .picture__close-btn {
+        display: none;
+        position: absolute;
+        right: 10px;
+        top: 10px;
     }
 </style>
